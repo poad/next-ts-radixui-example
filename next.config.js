df6220753aa334d30b29bf49559552a594174451
Kenji Saito <1867845+poad@users.noreply.github.com>
2023-02-26 16:13:53 +0900

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const config =  {
+  reactStrictMode: true,
+  swcMinify: true,
+  cleanDistDir: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = config;
