ff01570e0964c31eada277489c6819fe6ea654df
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-27 20:27:54 +0900

    Bumps node modules

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const config =  {
   images: {
     unoptimized: true,
   },
+  // Module not found: Can't resolve 'msw/browser' が解決するまで
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        'msw/browser': 'node_modules/msw/lib/browser',
+        'msw/node': 'node_modules/msw/lib/node',
+      },
+    },
+  },
 };
 
 module.exports = config;
