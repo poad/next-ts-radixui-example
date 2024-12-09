e75cb3f7b592aafde7bc3254b639fc2e48b0d6e2
Kenji Saito <ken-yo@mbr.nifty.com>
2024-12-09 22:37:40 +0900

    Bumps node modules

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const config =  {
   reactStrictMode: true,
-  swcMinify: true,
   cleanDistDir: true,
   images: {
     unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   // Module not found: Can't resolve 'msw/browser' が解決するまで
   experimental: {
